68e689751a32c33e140b68722a54f68acc17d46b
software engineering <167783381+software-engineering-projects@users.noreply.github.com>
2026-04-22 12:27:56 +0800

    Create 03 passing a function to another function.js

---
+++ programming concepts/functions/03 passing a function to another function.js
@@ -0,0 +1,55 @@
+function metaData(name, age, sex) {
+
+     return {
+          name: name,
+          age: age,
+          sex: sex
+       };
+     }
+
+function dataCheck(data) {
+
+     let normalizesex = data.sex.toLowerCase();
+     const sexcat = ['male', 'female']
+
+     if (typeof data.name !== "string") {
+          console.log(`${data.name} is not a valid name`);
+          return "Error";
+        } else {
+          console.log(`${data.name} is a valid name`);
+        }
+     if (typeof data.age !== "number") {
+          console.log(`${data.age} is not a valid age`);
+          return "Error";
+
+        } else {
+          console.log(`${data.age} is a valid age`);
+       }  
+     if (sexcat.includes(normalizesex)) {
+          console.log(`${normalizesex} is a valid category`);
+          return "Success";
+       } else {
+          console.log(`${normalizesex} is not a valid category`);
+          return "Error";
+       }
+    }
+
+
+function callFunction(arg1, arg2) {
+
+     const data = arg1;
+     const validate = arg2;
+
+     if (validate(data) === "Success") {
+          console.log("data is valid");
+     } else {
+          console.log("data is invalid");
+     }
+   }       
+
+const data = metaData('John', 24, 'FEmaLe');
+
+callFunction(
+     data,
+     dataCheck
+)
