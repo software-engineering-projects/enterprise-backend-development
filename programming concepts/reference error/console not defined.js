7010128e9f5decfd719ef83986b88767499500ce
software engineering <167783381+software-engineering-projects@users.noreply.github.com>
2026-04-22 10:49:02 +0800

    Update console not defined.js

--- programming concepts/reference error/console not defined.js
+++ programming concepts/reference error/console not defined.js
@@ -13,4 +13,12 @@ Stack trace:
 ReferenceError: comsole is not defined
     at checkUser (<anonymous>:5:3)
     at <anonymous>:7:1
+
+EXPLANATION:
+
+WRONG
+• Console.log("test");
+
+CORRECT
+• console.log("test");
 */
